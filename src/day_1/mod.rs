91f63667f458d7ee654eb975d007d945a4deb083
Seth Samuel <sethfsamuel@gmail.com>
2018-12-01 12:02:00 -0500

    Day 1 part 2

--- src/day_1/mod.rs
+++ src/day_1/mod.rs
@@ -5,7 +5,7 @@ pub fn frequency() {
 
     println!("File size read {}", input.len());
 
-    let lines = input.split("\n");
+    let lines = input.split('\n');
 
     let frequency = lines.fold(0, |current, line| {
         current + line
@@ -14,4 +14,34 @@ pub fn frequency() {
     });
 
     println!("Final frequency: {}", frequency);
+}
+
+use std::collections::HashMap;
+pub fn frequency_match() {
+    let input = include_str!("input.txt");
+    let mut current = 0;
+    let mut found = false;
+    let mut loop_count = 0;
+    let mut hash: HashMap<i32, bool> = HashMap::new();
+    hash.insert(current, true);
+    while !found {
+        let lines = input.split('\n');
+        for line in lines {
+            current += line
+                .parse::<i32>()
+                .unwrap_or_else(|err| panic!("Failed to parse line! {}", err));
+            if hash.get(&current).is_some() {
+                //FOUND!
+                found = true;
+                println!(
+                    "First repeated frequency {} after {} loops",
+                    current, loop_count
+                );
+                break;
+            } else {
+                hash.insert(current, true);
+            }
+        }
+        loop_count += 1;
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -3,5 +3,6 @@ mod day_1;
 fn main() {
     println!("Starting...");
 
-    day_1::frequency();
+    // day_1::frequency();
+    day_1::frequency_match();
 }
